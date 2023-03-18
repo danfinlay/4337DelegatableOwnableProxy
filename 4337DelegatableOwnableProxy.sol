2c22a55ea730111e10dbd826c85b1e5a8ce4c38f
Dan Finlay <dan@danfinlay.com>
2023-03-17 20:23:30 -0700

    GPT4 minimal ownable proxy contract

---
+++ 4337DelegatableOwnableProxy.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
+
+contract OwnableProxy {
+    address public implementation;
+    address public owner;
+
+    constructor(address _implementation) {
+        implementation = _implementation;
+        owner = msg.sender;
+    }
+
+    fallback() external payable {
+        address _impl = implementation;
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), _impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        Ownable(implementation).transferOwnership(newOwner);
+    }
+}
+
